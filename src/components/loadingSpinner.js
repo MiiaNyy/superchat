118eb99633cdc71edfcafc672eca29907f1d38c6
Miia Nyy <66413480+MiiaNyy@users.noreply.github.com>
2021-08-30 15:04:56 +0300

    Updates

--- src/components/loadingSpinner.js
+++
@@ -1,9 +0,0 @@
-import spinner from "../assets/spinner.svg";
-import React from "react";
-
-function LoadingSpinner() {
-    return <img width={ 90 } height={ 90 } style={ {margin: "2em auto", gridArea: "1/2/2/2"} } src={ spinner }
-                alt="loading"/>
-}
-
-export default LoadingSpinner;
--- src/helpers/getUsersLastVisit.js
+++
@@ -1,7 +0,0 @@
-function getUsersLastVisit(user) {
-    if ( user.lastSeen ) {
-        return Number(user.lastSeen.seconds + '.' + user.lastSeen.nanoseconds);
-    }
-}
-
-export default getUsersLastVisit;
